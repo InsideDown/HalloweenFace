da0c6fbd821a9a08b7accfe53b0a16b1f369560c
Paul Mealy <paul@inisdedown.com>
2020-10-07 11:04:09 -0400

    - adding face stuff to jack o lantern

---
+++ Assets/JackoLantern/Scripts/DisablePumpkin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisablePumpkin : MonoBehaviour
+{
+    public GameObject DisableObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(DisableObject != null)
+            DisableObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
